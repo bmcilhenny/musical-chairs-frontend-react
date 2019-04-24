242ffda420ab80dd8251280e7d39fd96449f7da9
bmcilhenny <bmcilhen@gmail.com>
2019-04-24 18:24:55 -0400

    reveal now working, for some reason if you have an image as a link it throws off styling

--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Image, Grid, Button, Header} from 'semantic-ui-react';
+import { Image, Grid, Button, Reveal, Header} from 'semantic-ui-react';
 
 const PlaylistCard = (props) => {
     const {playlist, handleOpen} = props;
     return (
         <Grid.Column>
-            <div onClick={handleOpen}>
-                <Image  src={playlist.imageUrl} style={{cursor: 'pointer'}} size='medium' as='a'/>
-                <Header as='h5' style={{color: 'white', marginBlockStart: '0.5em'}}>
-                {playlist.name}
-                    <Header.Subheader style={{color: '#969696'}}>{playlist.total} song{playlist.total > 1 ? 's' : null}</Header.Subheader>
-                </Header>
-            </div>
+                <Reveal animated='fade' onClick={handleOpen}>
+                    <Reveal.Content visible>
+                        <Image  src={playlist.imageUrl} size='medium'/>
+                    </Reveal.Content>
+                    <Reveal.Content hidden>
+                        <Image  src={playlist.imageUrl} size='medium'/>
+                    </Reveal.Content>
+                </Reveal>
+                    <Header as='h5' style={{color: 'white', marginBlockStart: '0.5em'}}>
+                        {playlist.name}
+                        <Header.Subheader style={{color: '#969696'}}>{playlist.total} song{playlist.total > 1 ? 's' : null}</Header.Subheader>
+                    </Header>
         </Grid.Column>
     )
  }
