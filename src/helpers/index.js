21356971391c2e0a59d81de9736cd37214657e50
bmcilhenny <bmcilhen@gmail.com>
2019-04-26 22:21:22 -0400

    remove unused, add tokenExpired function to helpers so I can call it in the withAuth getDerivedStateFromProps lifecycle method cause I don't have access to this inside it and therefore can't use an instant method I defined on it

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,4 +2,4 @@ export const sleep = m => new Promise(r => setTimeout(r, m))
 
 export const genRandomNumber = (max, min) => Math.floor(Math.random() * ((max-min)+1) + min);
 
-// export const formatName = (str) => str.length > 
+export const tokenExpired = (expires) => (Date.now - expires) > 3500000 ? true : false
