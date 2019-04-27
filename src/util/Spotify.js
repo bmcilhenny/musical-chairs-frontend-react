865c5adb6eff291a6c02d7adb6bf2d50ac095b1f
bmcilhenny <bmcilhen@gmail.com>
2019-04-26 22:22:34 -0400

    add methods to util Spotify file for dryer code, essentially using the same function in the withAuth componentDidMount as well as the getDerivedStateFromProps lifecycle methods

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,28 @@
 import { cleanPlaylistData } from './DataCleaner';
+import {tokenExpired} from '../helpers';
+
+export const setUpSpotifyAuthorization = () => {
+  const client_id = process.env.SPOTIFY_CLIENT_ID || '5f8edf6fa5254fb8ae8f9ff4839e8d4c';
+  const scopes = encodeURIComponent('user-read-playback-state user-modify-playback-state playlist-read-private');
+  const redirect_uri = window.location.href;
+  const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${scopes}&show_dialog=true&redirect_uri=${redirect_uri}`;
+  window.location = url;
+}
+
+export const handleSpotifyToken = (spotify, props) => {
+  let spotifyAccessToken = JSON.parse(localStorage.getItem('spotify-access-token'));
+  if (spotifyAccessToken) {
+      const {token, expires} = spotifyAccessToken;
+      if ((tokenExpired(expires))) {
+          props.history.push('/login')
+      } else {
+          spotify.setAccessToken(token)
+      }
+  } else {
+      props.history.push('/login')
+  }
+}
+
 
 export const getPaginatedPlaylists = async (spotify, playlists, limit=50, offset=0) => {
   try {
