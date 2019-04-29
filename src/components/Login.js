de842f5edd79e042b01ff5b62f44a751e5e9f149
bmcilhenny <bmcilhen@gmail.com>
2019-04-29 11:25:25 -0400

    make Login a stateful component that handles the redirect with the (relatively) new component lifecycle method

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,31 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
+import { setUpSpotifyAuthorization } from '../util/Spotify';
 import { Button } from 'semantic-ui-react';
+import QueryString from 'querystring';
 
-const Login = ({handleLogin}) => {
-    return (
-        <Fragment>
-           <Button positive onClick={handleLogin}>Log in</Button>
-        </Fragment>
-    )
+
+class Login extends Component {
+    state = {}
+
+    static getDerivedStateFromProps(nextProps) { 
+        if (window.location.hash.length > 1) {
+        const querystring = window.location.hash.slice(1);
+        const query = QueryString.parse(querystring);
+            if (query.access_token) {
+            localStorage.setItem('spotify-access-token', JSON.stringify({token: query.access_token, expires: Date.now()}));
+            nextProps.history.push('/home')
+            }
+        }
+        return null;
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Button positive onClick={setUpSpotifyAuthorization}>Login</Button>
+            </Fragment>
+        )
+    }
 }
 
 export default Login;
