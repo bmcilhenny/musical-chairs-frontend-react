4f010df628c31e274b8807aae83363b17a9b3f14
bmcilhenny <bmcilhen@gmail.com>
2019-04-29 14:58:06 -0400

    remove segment from App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component } from 'react';
 import {Route, Switch } from 'react-router-dom';
-import { Segment  } from 'semantic-ui-react';
 import {withRouter, Redirect} from 'react-router';
-// import { handleRedirectResponse} from './util/Spotify';
 import Loadable from 'react-loadable';
 import Login from './components/Login'
  
@@ -16,13 +14,11 @@ class App extends Component {
 
   render() {
     return (
-        <Segment inverted>
           <Switch>
             <Route exact path='/login' render={(routerProps) => < Login {...routerProps} />} />
             <Route exact path='/home' render={(routerProps) =>  <Home {...routerProps} /> } />
             <Route path='*' render={() => <Redirect to='/home' />} />
           </Switch>
-        </Segment>
     )
 
   }
