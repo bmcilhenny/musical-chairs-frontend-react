4c90d12fb93e009c005c13fceaf2170e48951c92
bmcilhenny <bmcilhen@gmail.com>
2019-05-06 13:37:16 -0400

    customize styling for About link because of react scroll and semantic clashes

--- src/components/login/Footer.js
+++ src/components/login/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import {Segment, Container, Grid, Header, List } from 'semantic-ui-react';
+import { GITHUB_URL } from '../../constants';
 
 const Footer = () => {
     return (
@@ -10,23 +12,22 @@ const Footer = () => {
                         <Grid.Column width={3}>
                         <Header inverted as='h4' content='About' />
                         <List link inverted>
-                            <List.Item as='a'>Home</List.Item>
-                            <List.Item as='a'>About</List.Item>
-                            <List.Item as='a'>Github</List.Item>
+                            <List.Item as='a' href='/home'>Home</List.Item>
+                            <div className='footerAbout'>
+                                    <Link  className='footerAbout' to="About Section" spy={true} smooth={true} duration={1000}>
+                                        About
+                                    </Link>
+                                {/* </List.Item> */}
+                            </div>
+                            <List.Item as='a' href={GITHUB_URL} >Github</List.Item>
                         </List>
                         </Grid.Column>
-                        <Grid.Column width={3}>
-                        <Header inverted as='h4' content='Services' />
-                        <List link inverted>
-                            <List.Item as='a'>None</List.Item>
-                        </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
+                        <Grid.Column width={13}>
                         <Header as='h4' inverted>
                             A Note to the Reader
                         </Header>
                         <p>
-                            Thanks for making it this far! I am looking for a Full Stack position on a team that takes pride in collaboration and mentorship.
+                            Thanks for making it this far! I am looking for a Full Stack position on a team that takes pride in collaborating and mentorship.
                         </p>
                         </Grid.Column>
                     </Grid.Row>
