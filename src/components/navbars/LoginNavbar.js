84373b43023056ff7b49d28dcf4a0e6c147ce38f
bmcilhenny <bmcilhen@gmail.com>
2019-05-07 10:17:30 -0400

    conditionally render dropdown menu if user is already logged in, as in the user object is in local storage, otherwise render the log in button that handles spotify login

--- src/components/navbars/LoginNavbar.js
+++ src/components/navbars/LoginNavbar.js
@@ -2,33 +2,37 @@ import React from 'react';
 import { Link } from 'react-scroll';
 import {Menu, Dropdown, Image, Header, Icon } from 'semantic-ui-react';
 import { FINDING_NEMO_URL, GITHUB_URL } from '../../constants';
+import { setUpSpotifyAuthorization } from '../../util/Spotify';
 
 
-const LoginNavbar = () => {
+const LoginNavbar = ({authorized, user}) => {
     return (
         <Menu attached='top' style={{backgroundColor: 'transparent'}} borderless inverted >
-            <Menu.Item position='left'>
+            <Menu.Item position='left' >
                 <Header as='h3' inverted >
-                    <Icon name='beer' /> Musical Cheers
+                    <Icon name='beer'/> Musical Cheers
                 </Header>
             </Menu.Item>
             <Menu.Menu position='right'>
-                <Menu.Item name='home' href={'/login'} />
+                <Menu.Item name='home' href={'/home'} />
                 <Menu.Item className='menuHover'>
                     <Link activeClass="active" to="About Section" spy={true} smooth={true} duration={1000}>
                         About
                     </Link>
                 </Menu.Item>
                 <Menu.Item name='github' href={GITHUB_URL} />
-                <Menu.Item>
-                    <Image avatar src={FINDING_NEMO_URL} />
-                    <Dropdown text='Profile' pointing='top right'>
-                        <Dropdown.Menu >
-                            <Dropdown.Item text='Settings' />
-                            <Dropdown.Item text='Log out' />
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Menu.Item>
+                {user.images ?
+                    <Menu.Item>
+                        <Image avatar src={user.images[0].url} />
+                        <Dropdown text={`Welcome, ${user.display_name}`} pointing='top right'>
+                            <Dropdown.Menu >
+                                <Dropdown.Item text='Settings' />
+                                <Dropdown.Item text='Log out' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item> :
+                    <Menu.Item name='log in' onClick={setUpSpotifyAuthorization} />
+                 }
             </Menu.Menu>
         </Menu>   
     )
