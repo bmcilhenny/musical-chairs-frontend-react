2736abf84238e8d19dec265536ec9a8326d81209
bmcilhenny <bmcilhen@gmail.com>
2019-05-08 22:42:23 -0400

    nothing

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,4 +2,4 @@ export const sleep = m => new Promise(r => setTimeout(r, m))
 
 export const genRandomNumber = (max, min) => Math.floor(Math.random() * ((max-min)+1) + min);
 
-export const tokenExpired = (expires) => (Date.now() - expires) > 3500000 ? true : false
+export const tokenExpired = (expires) => (Date.now() - expires) > 3500000 ? true : false;
