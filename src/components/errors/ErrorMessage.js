02f2c042b945cc3e5728d267b375ccd35c510995
bmcilhenny <bmcilhen@gmail.com>
2019-05-08 22:41:46 -0400

    moved error file to own folder

--- src/components/errors/ErrorMessage.js
+++ src/components/errors/ErrorMessage.js
@@ -4,7 +4,7 @@ import {Message} from 'semantic-ui-react';
 const ErrorMessage = ({error}) => {
     return (
         <Message negative>
-            <Message.Header>There was an error fetching some data.</Message.Header>
+            <Message.Header>There was an error fetching some data. </Message.Header><span>Try refreshing the page.</span>
             <p>{error.message}</p>
         </Message>
     )
